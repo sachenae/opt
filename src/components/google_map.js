5715c45401507fe311d8ddd0d1fde02c8190b412
sachins <39sachen@gmail.com>
2018-06-18 17:50:29 +0300

    map error

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,21 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
+const MyMapComponent = withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+  </GoogleMap>
+)
 
-
-class Map extends Component {
-
-render() {
- const markers = this.props.markers || []
-    return (
-      <GoogleMap
-      defaultZoom={8}
-      defaultCenter={{ lat: -34.397, lng: 150.644 }}
-    >
-      {markers.map((marker, index)=>(<Marker position={{ lat: -34.397, lng: 150.644 }} />)) }
-    </GoogleMap>
-    )
-  }
-}
-
-export default withGoogleMap(Map)
