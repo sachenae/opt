b1f239df9577606c9866f222c9eec5227ea82153
sachins <39sachen@gmail.com>
2018-06-26 14:44:07 +0300

    changed server.js

--- server.js
+++ server.js
@@ -1,39 +1,55 @@
-const {createServer} = require('http');
-const express = require('express');
-const compression = require('compression');
-const morgan = require('morgan');
-const path = require('path');
+// const {createServer} = require('http');
+// const express = require('express');
+// const compression = require('compression');
+// const morgan = require('morgan');
+// const path = require('path');
 
-const normalizePort = port => parseInt(port, 10);
-const PORT = normalizePort(process.env.PORT|| 5000);
+// const normalizePort = port => parseInt(port, 10);
+// const PORT = normalizePort(process.env.PORT|| 5000);
 
-const app = express();
-const dev = app.get('env') !== 'production';
+// const app = express();
+// const dev = app.get('env') !== 'production';
 
 
-if(!dev){
-    app.disable('x-powered-by');
-    app.use(compression());
-    app.use(morgan('common'));
+// if(!dev){
+//     app.disable('x-powered-by');
+//     app.use(compression());
+//     app.use(morgan('common'));
 
-    app.use(express.static(path.resolve(__dirname, 'build')))
+//     app.use(express.static(path.resolve(__dirname, 'build')))
 
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-    });
+//     app.get('*', (req,res) => {
+//         res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+//     });
     
-}
+// }
 
-if(dev) {
-    app.use(morgan('dev'));
-}
-
-
-const server = createServer(app);
+// if(dev) {
+//     app.use(morgan('dev'));
+// }
 
 
+// const server = createServer(app);
 
-server.listen(PORT, err => {
-    if(err) throw err;
-    console.log('Server started');
+
+
+// server.listen(PORT, err => {
+//     if(err) throw err;
+//     console.log('Server started');
+// });
+
+
+
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'))
 });
+
+app.listen(port);
+console.log('Server started');
