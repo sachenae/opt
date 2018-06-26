f8aa6a6fbafddf13576d571216a61ecaef8d536a
sachins <39sachen@gmail.com>
2018-06-26 15:13:02 +0300

    final

--- server.js
+++ server.js
@@ -1,55 +1,41 @@
-// const {createServer} = require('http');
-// const express = require('express');
-// const compression = require('compression');
-// const morgan = require('morgan');
-// const path = require('path');
-
-// const normalizePort = port => parseInt(port, 10);
-// const PORT = normalizePort(process.env.PORT|| 5000);
-
-// const app = express();
-// const dev = app.get('env') !== 'production';
-
-
-// if(!dev){
-//     app.disable('x-powered-by');
-//     app.use(compression());
-//     app.use(morgan('common'));
-
-//     app.use(express.static(path.resolve(__dirname, 'build')))
-
-//     app.get('*', (req,res) => {
-//         res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-//     });
-    
-// }
-
-// if(dev) {
-//     app.use(morgan('dev'));
-// }
-
-
-// const server = createServer(app);
-
-
-
-// server.listen(PORT, err => {
-//     if(err) throw err;
-//     console.log('Server started');
-// });
-
-
-
+const {createServer} = require('http');
 const express = require('express');
+const compression = require('compression');
+const morgan = require('morgan');
 const path = require('path');
-const port = process.env.PORT || 8080;
+
+const normalizePort = port => parseInt(port, 10);
+const PORT = normalizePort(process.env.PORT|| 5000);
+
 const app = express();
+const dev = app.get('env') !== 'production';
 
-app.use(express.static(__dirname));
 
-app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'))
+if(!dev){
+    app.disable('x-powered-by');
+    app.use(compression());
+    app.use(morgan('common'));
+
+    app.use(express.static(path.resolve(__dirname, 'build')));
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
+    
+}
+
+if(dev) {
+    app.use(morgan('dev'));
+}
+
+
+const server = createServer(app);
+
+
+
+server.listen(PORT, err => {
+    if(err) throw err;
+    console.log('Server started');
 });
 
-app.listen(port);
-console.log('Server started');
+
